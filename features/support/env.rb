524401227dc4721290fe4c29004c1ff2ed54491e
ghfig2 <ghfig@hotmail.com>
2016-11-11 02:29:30 -0200

    Test execution conditions

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,14 +3,48 @@ require 'site_prism'
 require "selenium-webdriver"
 require 'capybara/cucumber'
 require 'capybara/rspec'
+require 'faker'
+
+Faker::Config.locale = 'pt-BR'
 
 Capybara.default_driver = :selenium
 Capybara.app_host = 'https://www.youse.com.br'
-
+Capybara.default_max_wait_time = 60
 Capybara.register_driver :selenium do |app|
   Capybara::Selenium::Driver.new(app, :browser => :chrome)
 end
 
+
+##############################################################################
+##############################################################################
+
 # Variables passed by parameter on the command line
-$login = ENV['email']
+$email = ENV['email']
 $password = ENV['password']
+$name = ENV['name'] || Faker::Name.name
+
+# Tests won't run if email and password are missing
+if (!$email || !$password)
+	puts "E-mail and password are mandatory. Run again passing them as parameter: 'cucumber email=name@site.com password=12345678'"
+	exit
+end
+
+
+# Password must have length more than 7
+if ($password.length < 8)
+	puts "Password must have more than 7 characters"
+	exit
+end
+
+##############################################################################
+##############################################################################
+
+
+Before do
+	page.driver.browser.manage.window.maximize
+end
+
+
+After do
+  Capybara.reset_sessions!
+end
